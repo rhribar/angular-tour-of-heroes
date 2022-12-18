003747ab4b6de8c66caf0a94053c75ae5c07aa43
Rok Hribar <zbd@eranovum.partners>
2022-12-18 15:25:05 +0100

    first commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  heroes: Hero[] = []; // binds it to this component
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes); // async through http client
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe(); // you must always subscribe even when you have nothing to do with it
+  }
+}
